7c377926fe848fcebd5f3521ae3f319687533a9a
dobriak <dobriak@gmail.com>
2015-06-23 14:44:22 -0700

    Initial commit

---
+++ bash/capture_cdp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## This will often show you the Cisco chassis switch, then use your firms asset management software to find the upstream switch.
+## -s 1500 capture 1500 bytes of the packet (typical MTU size)
+## ether[20:2] == 0x2000 - Capture only packets that are starting at byte 20, and have a 2 byte value of hex 2000 
+## See http://en.wikipedia.org/wiki/Cisco_Discovery_Protocol
+
+tcpdump -v -s 1500 -c 1 'ether[20:2] == 0x2000'
