2a16068b33177429c0cccc17a6740d4c6cc2107a
Julian Neytchev <dobriak@gmail.com>
2015-10-19 11:43:27 -0700

    Added interface param to the second line

--- bash/capture_lldp.sh
+++ bash/capture_lldp.sh
@@ -4,4 +4,4 @@
 tcpdump -i eth0 -s 1500 -XX -c 1 'ether proto 0x88cc'
  
 ## Port and CDP Neighbor Info:
-tcpdump -v -s 1500 -c 1 '(ether[12:2]=0x88cc or ether[20:2]=0x2000)'
+tcpdump -i eth0 -v -s 1500 -c 1 '(ether[12:2]=0x88cc or ether[20:2]=0x2000)'
