076ee93520295a6d9d9b785af95399ba59325744
Julian Neytchev <dobriak@gmail.com>
2016-01-11 15:12:52 -0800

    Added mass ping script
    Simplistic mass pinging with formatting of the output.

---
+++ bash/mass_ping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Easy mass pinging
+
+function mass_ping(){
+    local network=${1}
+    local from=${2}
+    local to=${3}
+    local pingable=()
+    echo "Pingable IPs ${network}.${from} - ${network}.${to}"
+
+    for i in $(seq ${from} ${to}); do
+        echo -ne "Pinging ${network}.${i}...\033[0K\r"
+        if  ping -c 2 ${network}.${i} &> /dev/null; then
+            pingable+=("${network}.${i}")
+        fi
+    done
+    echo -e "\033[2K"
+    for i in "${pingable[@]}"; do
+        echo ${i}
+    done
+}
+
+mass_ping 10.22.18 2 254
+mass_ping 172.16.106 2 254
+mass_ping 192.168.30 2 254
